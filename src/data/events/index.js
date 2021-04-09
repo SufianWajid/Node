7e092d80a7fb955e4ae55f0c0f5387222fe39a15
SufianWajid <m.sufianwajid@gmail.com>
2021-04-09 15:12:17 +0500

    Update index.js

--- src/data/events/index.js
+++ src/data/events/index.js
@@ -1,28 +1,28 @@
 "use strict";
 
-const utils = require( "../utils" );
+const utils = require("../utils");
 
-const register = async ( { sql, getConnection } ) => {
-   // read in all the .sql files for this folder
-   const sqlQueries = await utils.loadSqlQueries( "events" );
+const register = async ({ sql, getConnection }) => {
+  // read in all the .sql files for this folder
+  const sqlQueries = await utils.loadSqlQueries("events");
 
-   const getEvents = async text => {
-       // get a connection to SQL Server
-       const cnx = await getConnection();
+  const getEvents = async (text) => {
+    // get a connection to SQL Server
+    const cnx = await getConnection();
 
-       // create a new request
-       const request = await cnx.request();
+    // create a new request
+    const request = await cnx.request();
 
-       // configure sql query parameters
-       request.input( "text", sql.VarChar( 50 ), text );
+    // configure sql query parameters
+    request.input("text", sql.VarChar(50), text.NAM);
 
-       // return the executed query
-       return request.query( sqlQueries.getEvents );
-   };
+    // return the executed query
+    return request.query(sqlQueries.getEvents);
+  };
 
-   return {
-       getEvents
-   };
+  return {
+    getEvents,
+  };
 };
 
 module.exports = { register };
