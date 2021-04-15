6dd2554f8b08206db9cd6e633d86f59b32ee7b05
SufianWajid <m.sufianwajid@gmail.com>
2021-04-15 16:53:39 +0500

    Update events.js

--- src/routes/api/events.js
+++ src/routes/api/events.js
@@ -14,7 +14,7 @@ module.exports.register = async (server) => {
           // TODO: Get the current authenticate user's ID
           const Id = 1;
           const { text } = request.payload;
-          console.log(text);
+          console.log("New log" + text);
 
           // execute the query
           // const res = await db.events.getEvents(text);
