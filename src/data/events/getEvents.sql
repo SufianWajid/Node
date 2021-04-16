dfddc22d86b6da39a57c7af249b6b0bd23d9b814
SufianWajid <m.sufianwajid@gmail.com>
2021-04-16 17:31:16 +0500

    Update getEvents.sql

--- src/data/events/getEvents.sql
+++ src/data/events/getEvents.sql
@@ -1,5 +1,5 @@
-INSERT INTO TestData(Text,CreateDate,User_Name)
-VALUES (@text,@date,@currentUser);
+-- INSERT INTO TestData(Text,CreateDate,User_Name)
+-- VALUES (@text,@date,@currentUser);
 
 -- IF (NOT EXISTS(SELECT * FROM TestData WHERE Text=@text AND User_Name=@currentUser)) 
 -- BEGIN 
@@ -12,3 +12,15 @@ VALUES (@text,@date,@currentUser);
 -- SET CreateDate=@date, Audio=@textCode
 -- WHERE Text=@text AND User_Name=@currentUser
 -- END 
+
+IF (NOT EXISTS(SELECT * FROM TestData WHERE SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) AND User_Name=@currentUser)) 
+BEGIN 
+    INSERT INTO TestData(Text,CreateDate,User_Name) 
+    VALUES (@text,@date,@currentUser)
+END 
+ELSE 
+BEGIN 
+    UPDATE TestData 
+SET CreateDate=@date
+WHERE SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) AND User_Name=@currentUser
+END 
