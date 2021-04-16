cbe54d51eeab38de766f81294fd9c7c27cdc8c5d
SufianWajid <m.sufianwajid@gmail.com>
2021-04-16 16:53:07 +0500

    Revert "Update getEvents.sql"
    This reverts commit 43c79e28abdedc3cabff212c3e35d261724bff9b.

--- src/data/events/getEvents.sql
+++ src/data/events/getEvents.sql
@@ -1,20 +1,7 @@
-INSERT INTO TestData(Text,Audio,CreateDate,User_Name)
-VALUES (@text,@textCode,@date,@currentUser);
+-- INSERT INTO TestData(Text,Audio,CreateDate,User_Name)
+-- VALUES (@text,@textCode,@date,@currentUser);
 
-IF (NOT EXISTS(SELECT * FROM TestData WHERE Text=@text AND User_Name=@currentUser)) 
-BEGIN 
-    INSERT INTO TestData(Text,Audio,CreateDate,User_Name) 
-    VALUES (@text,@textCode,@date,@currentUser)
-END 
-ELSE 
-BEGIN 
-    UPDATE TestData 
-SET CreateDate=@date, Audio=@textCode
-WHERE Text=@text AND User_Name=@currentUser
-END 
-
-
--- IF (NOT EXISTS(SELECT * FROM TestData WHERE SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) AND User_Name=@currentUser)) 
+-- IF (NOT EXISTS(SELECT * FROM TestData WHERE Text=@text AND User_Name=@currentUser)) 
 -- BEGIN 
 --     INSERT INTO TestData(Text,Audio,CreateDate,User_Name) 
 --     VALUES (@text,@textCode,@date,@currentUser)
@@ -23,5 +10,18 @@ END
 -- BEGIN 
 --     UPDATE TestData 
 -- SET CreateDate=@date, Audio=@textCode
--- WHERE SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) AND User_Name=@currentUser
+-- WHERE Text=@text AND User_Name=@currentUser
 -- END 
+
+
+IF (NOT EXISTS(SELECT * FROM TestData WHERE SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) AND User_Name=@currentUser)) 
+BEGIN 
+    INSERT INTO TestData(Text,Audio,CreateDate,User_Name) 
+    VALUES (@text,@textCode,@date,@currentUser)
+END 
+ELSE 
+BEGIN 
+    UPDATE TestData 
+SET CreateDate=@date, Audio=@textCode
+WHERE SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) AND User_Name=@currentUser
+END 
