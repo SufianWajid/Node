d512d85efe6e604ac2b4ab49428fbfdab7f6fcef
SufianWajid <m.sufianwajid@gmail.com>
2021-04-16 10:01:30 +0500

    date update

--- src/routes/api/events.js
+++ src/routes/api/events.js
@@ -21,7 +21,23 @@ module.exports.register = async (server) => {
             this.Code = Code;
             this.date = date;
           }
-          var obj = new rowObj(text.NAM, text.Code, text.date);
+
+          let d = text.date;
+
+          d =
+            d.getFullYear() +
+            "-" +
+            ("00" + (d.getMonth() + 1)).slice(-2) +
+            "-" +
+            ("00" + d.getDate()).slice(-2) +
+            " " +
+            ("00" + d.getHours()).slice(-2) +
+            ":" +
+            ("00" + d.getMinutes()).slice(-2) +
+            ":" +
+            ("00" + d.getSeconds()).slice(-2);
+
+          var obj = new rowObj(text.NAM, text.Code, d);
 
           // execute the query
           const res = await db.events.getEvents(obj);
