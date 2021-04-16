017ca2b1595356bf58c6033c4d1ce79b46dd3c8a
SufianWajid <m.sufianwajid@gmail.com>
2021-04-16 16:11:04 +0500

    Update getEvents.sql

--- src/data/events/getEvents.sql
+++ src/data/events/getEvents.sql
@@ -14,14 +14,14 @@
 -- END 
 
 
-IF (NOT EXISTS(SELECT * FROM TestData WHERE SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) OR Text=@text AND User_Name=@currentUser)) 
+IF (NOT EXISTS(SELECT * FROM TestData WHERE SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) AND User_Name=@currentUser)) 
 BEGIN 
-    INSERT INTO TestData(Text,CreateDate,User_Name) 
+    INSERT INTO TestData(Text,Audio,CreateDate,User_Name) 
     VALUES (@text,@textCode,@date,@currentUser)
 END 
 ELSE 
 BEGIN 
     UPDATE TestData 
 SET CreateDate=@date, Audio=@textCode
-WHERE SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) OR Text=@text AND User_Name=@currentUser
+WHERE SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) AND User_Name=@currentUser
 END 
