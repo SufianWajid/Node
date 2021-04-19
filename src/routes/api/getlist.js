ef563abcee3f046d4a2345753cb5e93c92f37e0a
SufianWajid <m.sufianwajid@gmail.com>
2021-04-19 13:24:36 +0500

    get added

---
+++ src/routes/api/getlist.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports.register = async (server) => {
+  server.route({
+    method: "GET",
+    path: "/api/getlist",
+
+    config: {
+      handler: async (request) => {
+        try {
+          // get the sql client registered as a plugin
+          const db = request.server.plugins.sql.client;
+
+          // TODO: Get the current authenticate user's ID
+          const Id = 1;
+          const { text } = request.payload;
+          console.log(text);
+
+          // execute the query
+          const res = await db.events.getEvents(obj);
+
+          // return the recordset object
+          return "Record Get";
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    },
+  });
+};
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,7 +1,9 @@
 "use strict";
 
-const events = require( "./events" );
+const events = require("./events");
+const getlist = require("./getlist");
 
-module.exports.register = async server => {
-   await events.register( server );
+module.exports.register = async (server) => {
+  await events.register(server);
+  await getlist.register(server);
 };
