735f005348d0a8a0466b702467119e5ae4a9341d
SufianWajid <m.sufianwajid@gmail.com>
2021-04-19 12:08:19 +0500

    update

--- src/data/events/getEvents.sql
+++ src/data/events/getEvents.sql
@@ -16,11 +16,11 @@
 IF (NOT EXISTS(SELECT * FROM TestData WHERE (SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) OR Text =@text) AND User_Name=@currentUser)) 
 BEGIN 
     INSERT INTO TestData(Text,CreateDate,User_Name,Audio) 
-    VALUES (@text,@date,@currentUser,@textCode)
+    VALUES (@text,@date,@currentUser,CONVERT(VARBINARY(25), @textCode, 1))
 END 
 ELSE 
 BEGIN 
     UPDATE TestData 
-SET CreateDate=@date, Audio=@textCode
+SET CreateDate=@date, Audio=CONVERT(VARBINARY(25), @textCode, 1)
 WHERE (SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) OR Text=@text )AND User_Name=@currentUser
 END 
