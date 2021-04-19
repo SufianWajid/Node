ef006738c970409148518e56c7d6788e2e358800
SufianWajid <m.sufianwajid@gmail.com>
2021-04-19 12:28:15 +0500

    Update getEvents.sql

--- src/data/events/getEvents.sql
+++ src/data/events/getEvents.sql
@@ -13,16 +13,20 @@
 -- WHERE Text=@text AND User_Name=@currentUser
 -- END 
 
-declare @str varchar(max) = @textCode;
+declare @source varbinary(max), @encoded varchar(max), @decoded varbinary(max)
+set @source = convert(varbinary(max), @textCode)
+set @encoded = cast(‘’ as xml).value(‘xs:base64Binary(sql:variable(“@source”))’, ‘varchar(max)’)
+set @decoded = cast(‘’ as xml).value(‘xs:base64Binary(sql:variable(“@encoded”))’, ‘varbinary(max)’)
+
 
 IF (NOT EXISTS(SELECT * FROM TestData WHERE (SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) OR Text =@text) AND User_Name=@currentUser)) 
 BEGIN 
     INSERT INTO TestData(Text,CreateDate,User_Name,Audio) 
-    VALUES (@text,@date,@currentUser,cast(N'' as xml).value('xs:base64Binary(sql:variable("@str"))', 'varbinary(max)'))
+    VALUES (@text,@date,@currentUser,@decoded)
 END 
 ELSE 
 BEGIN 
     UPDATE TestData 
-SET CreateDate=@date, Audio=cast(N'' as xml).value('xs:base64Binary(sql:variable("@str"))', 'varbinary(max)')
+SET CreateDate=@date, Audio=@decoded
 WHERE (SUBSTRING(Text, 1, 4)+SUBSTRING(Text, 6, 4)+SUBSTRING(Text, 11, 4)=SUBSTRING(@text, 1, 4)+SUBSTRING(@text, 6, 4)+SUBSTRING(@text, 11, 4) OR Text=@text )AND User_Name=@currentUser
 END 
