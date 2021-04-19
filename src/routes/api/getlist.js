099ada2fdceea2f866e7d316d5a29b32206074ea
SufianWajid <m.sufianwajid@gmail.com>
2021-04-19 13:34:40 +0500

    Update getlist.js

--- src/routes/api/getlist.js
+++ src/routes/api/getlist.js
@@ -9,20 +9,20 @@ module.exports.register = async (server) => {
       handler: async (request) => {
         try {
           // get the sql client registered as a plugin
-          const db = request.server.plugins.sql.client;
+          //   const db = request.server.plugins.sql.client;
 
-          // TODO: Get the current authenticate user's ID
-          const Id = 1;
-          const { text } = request.payload;
-          console.log(text);
+          //   // TODO: Get the current authenticate user's ID
+          //   const Id = 1;
+          //   const { text } = request.payload;
+          //   console.log(text);
 
-          // execute the query
-          const res = await db.events.getEvents(obj);
+          //   // execute the query
+          //   const res = await db.events.getEvents(obj);
 
           // return the recordset object
           return "Record Get";
         } catch (err) {
-          console.log(err);
+          console.log("11111");
         }
       },
     },
