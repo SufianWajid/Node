129810435052b8ddc8ded9970adbdd2036fe53e1
SufianWajid <m.sufianwajid@gmail.com>
2021-04-21 10:17:27 +0500

    Update getuserlist.sql

--- src/data/userdata/getuserlist.sql
+++ src/data/userdata/getuserlist.sql
@@ -1,2 +1,15 @@
+-- INSERT INTO AspNetUsers(Id,UserName,Email,EmailConfirmed,PasswordHash,PhoneNumber,PhoneNumberConfirmed)
+-- VALUES (NEWID(),@UserName,@Email,@EmailConfirmed,@PasswordHash,@PhoneNumber,@PhoneNumberConfirmed);
+
+
+IF (NOT EXISTS(SELECT * FROM AspNetUsers WHERE UserName=@UserName) 
+BEGIN 
 INSERT INTO AspNetUsers(Id,UserName,Email,EmailConfirmed,PasswordHash,PhoneNumber,PhoneNumberConfirmed)
-VALUES (NEWID(),@UserName,@Email,@EmailConfirmed,@PasswordHash,@PhoneNumber,@PhoneNumberConfirmed);
+VALUES (NEWID(),@UserName,@Email,@EmailConfirmed,@PasswordHash,@PhoneNumber,@PhoneNumberConfirmed)
+END 
+ELSE 
+BEGIN 
+    UPDATE AspNetUsers 
+SET Email=@Email,EmailConfirmed=@EmailConfirmed,PasswordHash=@PasswordHash,PhoneNumber=@PhoneNumber,PhoneNumberConfirmed=@PhoneNumberConfirmed
+WHERE UserName=@UserName
+END
