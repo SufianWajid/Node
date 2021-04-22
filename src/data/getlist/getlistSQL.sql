495ef4a3d818c40d3d6fc456cfddcefbed303f50
SufianWajid <m.sufianwajid@gmail.com>
2021-04-22 13:02:00 +0500

    Revert "Update getlistSQL.sql"
    This reverts commit 0d0de79adb8b0b46a3d2cfb26f71c66302c55d87.

--- src/data/getlist/getlistSQL.sql
+++ src/data/getlist/getlistSQL.sql
@@ -3,6 +3,5 @@ SELECT TOP (1000) [Id]
       ,[Audio]
       ,[User_Name]
       ,[CreateDate]
-      ,[IsSync]
   FROM [ConcilConnectTest].[dbo].[TestData]
   WHERE User_Name = @currentUser AND SUBSTRING(CreateDate, 1, 10)=SUBSTRING(@date, 1, 10)
