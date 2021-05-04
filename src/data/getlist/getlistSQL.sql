b21a9be0556ffe85afd6cf5158af5f5416ba8869
SufianWajid <m.sufianwajid@gmail.com>
2021-05-04 16:46:00 +0500

    Update getlistSQL.sql

--- src/data/getlist/getlistSQL.sql
+++ src/data/getlist/getlistSQL.sql
@@ -9,4 +9,4 @@ SELECT TOP (1000) [Id]
 
   FROM [ConcilConnectTest].[dbo].[TestData]
   WHERE User_Name = @currentUser AND SUBSTRING(CreateDate, 1, 10)=SUBSTRING(@date, 1, 10)
-ORDER BY Order# ASC
+ORDER BY Order_Number ASC
