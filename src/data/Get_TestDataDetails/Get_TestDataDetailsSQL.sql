38153f50f90b811d024ecf64bdc580dd0c7d2943
SufianWajid <m.sufianwajid@gmail.com>
2021-07-08 11:56:11 +0500

    TestDataDetails get SQL Updated

--- src/data/Get_TestDataDetails/Get_TestDataDetailsSQL.sql
+++ src/data/Get_TestDataDetails/Get_TestDataDetailsSQL.sql
@@ -2,13 +2,13 @@ SELECT [ID]
       ,[UserName]
       ,[NAMTextTestData]
       ,[CreateDateTestData]
-      ,[FormHeaderID]
       ,[FormDetailsDisplayedText]
       ,[Value_CheckBox]
       ,[Value_TextBox]
+      ,[FormHeaderNaming]
   FROM [ConcilConnectTest].[dbo].[TestDataDetails]
         WHERE LOWER(UserName)=LOWER(@UserName) AND
  LOWER(NAMTextTestData)=LOWER(@NAMTextTestData)  AND
- CreateDateTestData = CONVERT(datetime, @CreateDateTestData) AND
- FormHeaderID = @FormHeaderID AND
- LOWER(FormDetailsDisplayedText)=LOWER(@FormDetailsDisplayedText)
+ CreateDateTestData = CONVERT(date, @CreateDateTestData) 
+--  FormHeaderID = @FormHeaderID AND
+--  LOWER(FormDetailsDisplayedText)=LOWER(@FormDetailsDisplayedText)
