720630874830845d82a21edbc23ef50af043107e
SufianWajid <75831516+SufianWajid@users.noreply.github.com>
2021-09-17 19:45:58 +0500

    Update UpdateChatSQL.sql

--- src/data/UpdateChat/UpdateChatSQL.sql
+++ src/data/UpdateChat/UpdateChatSQL.sql
@@ -7,4 +7,5 @@ SELECT  [id]
       ,[UserAvatar]
       ,[createdAt]
   FROM [ConcilConnectTest].[dbo].[ChatsData]
-WHERE LOWER(UserName)=LOWER(@user) AND LOWER(UserId)=LOWER(@user) AND LOWER(SecondUser)=LOWER(@seconduser)
+WHERE (LOWER(UserName)=LOWER(@user) AND LOWER(UserId)=LOWER(@user) AND LOWER(SecondUser)=LOWER(@seconduser)) 
+OR (LOWER(UserName)=LOWER(@seconduser) AND LOWER(UserId)=LOWER(@seconduser) AND LOWER(SecondUser)=LOWER(@user))
